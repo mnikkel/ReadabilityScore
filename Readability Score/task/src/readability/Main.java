f31ae97bb7066dd9983efdad9b24c5971bc9faf5
Mark Nikkel <mnikkel@gmail.com>
2020-06-26 12:09:53 -0400

    complete

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -3,82 +3,147 @@ package readability;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
+        Text text = new Text(args[0]);
+
+        System.out.println("The text is:");
+        System.out.println("");
+        System.out.println("Words: " + text.words.length);
+        System.out.println("Sentences: " + text.sentences.length);
+        System.out.println("Characters: " + text.characterCount);
+        System.out.println("Syllables: " + text.syllables);
+        System.out.println("Polysyllables: " + text.polysyllables);
+        System.out.println("Enter the score you want to calculate (ARI, FK, SMOG, CL, all): ");
+
         try {
-            String text = readFileAsString(args[0]);
+            var scan = new Scanner(System.in);
+            String score = scan.nextLine();
+            int age;
+            switch (score) {
+                case "ARI":
+                    age = ages((int) Math.ceil(text.ari));
+                    System.out.printf("Automated Readability Index: %f (about %d year olds)%n", text.ari, age);
+                    break;
+                case "FK":
+                    age = ages((int) Math.ceil(text.fk));
+                    System.out.printf("Flesch–Kincaid readability tests: %f (about %d year olds)%n", text.fk, age);
+                    break;
+                case "SMOG":
+                    age = ages((int) Math.ceil(text.smog));
+                    System.out.printf("Simple Measure of Gobbledygook: %f (about %d year olds)%n", text.smog, age);
+                    break;
+                case "CL":
+                    age = ages((int) Math.ceil(text.cl));
+                    System.out.printf("Coleman–Liau index: %f (about %d year olds)%n", text.cl, age);
+                    break;
+                case "all":
+                    age = ages((int) Math.ceil(text.ari));
+                    System.out.printf("Automated Readability Index: %f (about %d year olds)%n", text.ari, age);
+                    age = ages((int) Math.ceil(text.fk));
+                    System.out.printf("Flesch–Kincaid readability tests: %f (about %d year olds)%n", text.fk, age);
+                    age = ages((int) Math.ceil(text.smog));
+                    System.out.printf("Simple Measure of Gobbledygook: %f (about %d year olds)%n", text.smog, age);
+                    age = ages((int) Math.ceil(text.cl));
+                    System.out.printf("Coleman–Liau index: %f (about %d year olds)%n", text.cl, age);
+                    break;
+                default:
+                    System.out.println("Unknown test");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-            String[] words = splitWords(text);
-            String[] sentences = splitSentences(text);
-            int characterCount = countCharacters(text);
-            double score = calculateReadability(characterCount, words.length, sentences.length);
-            String age = ages((int) Math.ceil(score));
+    }
 
-            System.out.println("The text is:");
-            System.out.println("");
-            System.out.println("Words: " + words.length);
-            System.out.println("Sentences: " + sentences.length);
-            System.out.println("Characters: " + characterCount);
-            System.out.println("The score is: " + score);
-            System.out.println("This text should be understood by " + age + " year olds.");
+    public static int ages(double score) {
+        if (score < 13) {
+            return (int) score + 6;
+        } else if (score == 13) {
+            return 24;
+        } else {
+            return 99;
+        }
+    }
+}
+
+class Text {
+    String text;
+    String[] words;
+    String[] sentences;
+    int characterCount;
+    int syllables;
+    int polysyllables;
+    double ari;
+    double fk;
+    double smog;
+    double cl;
+
+    Text(String filename) {
+        try {
+            text = readFileAsString(filename);
+            words = splitWords(text);
+            sentences = splitSentences(text);
+            characterCount = countCharacters(text);
+            setSyllables();
+            ari = calculateAri(characterCount, words.length, sentences.length);
+            fk = calculateFk(words.length, sentences.length, syllables);
+            smog = calculateSmog(polysyllables, sentences.length);
+            cl = calculateCl(characterCount, words.length, sentences.length);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static String ages(int score) {
-        switch (score) {
-            case 1:
-                return "5-6";
-            case 2:
-                return "6-7";
-            case 3:
-                return "7-9";
-            case 4:
-                return "9-10";
-            case 5:
-                return "10-11";
-            case 6:
-                return "11-12";
-            case 7:
-                return "12-13";
-            case 8:
-                return "13-14";
-            case 9:
-                return "14-15";
-            case 10:
-                return "15-16";
-            case 11:
-                return "16-17";
-            case 12:
-                return "17-18";
-            case 13:
-                return "18-24";
-            case 14:
-                return "24+";
-            default:
-                return "not defined";
-        }
+    double calculateCl(double characters, double words, double sentences) {
+        double l = (characters / words) * 100;
+        double s = (sentences / words) * 100;
+        return 0.0588 * l - 0.296 * s - 15.8;
     }
 
-    public static double calculateReadability(double chars, double words, double sentences) {
-        return (4.71 * (chars / words)) + (0.5 * (words / sentences)) - 21.43;
+    double calculateSmog(int polysyllables, double sentences) {
+        return 1.043 * Math.sqrt(polysyllables * (30 / sentences)) + 3.1291;
     }
 
-    public static String readFileAsString(String fileName) throws IOException {
+    String readFileAsString(String fileName) throws IOException {
         return new String(Files.readAllBytes(Paths.get(fileName)));
     }
 
-    public static int countCharacters(String text) {
-        return text.replaceAll("\\s", "").length();
+    void setSyllables() {
+        for (String word : words) {
+            int count = countSyllables(word);
+            syllables += count;
+            if (count > 2) {
+                polysyllables += 1;
+            }
+        }
     }
 
-    public static String[] splitWords(String text) {
+    int countSyllables(String word) {
+        Pattern vowels = Pattern.compile("([aiouyAIOUY]|e\\B|E\\B)+");
+        return (int) vowels.matcher(word).results().count();
+    }
+
+    String[] splitWords(String text) {
         return text.split("\\s+");
     }
 
-    public static String[] splitSentences(String text) {
+    String[] splitSentences(String text) {
         return text.split("[.?!]");
+    }
+
+    int countCharacters(String text) {
+        return text.replaceAll("\\s", "").length();
+    }
+
+    double calculateAri(double chars, double words, double sentences) {
+        return (4.71 * (chars / words)) + (0.5 * (words / sentences)) - 21.43;
+    }
+
+    double calculateFk(double words, double sentences, double syllables) {
+        return (0.39 * (words / sentences) + 11.8 * (syllables / words) - 15.59);
     }
 }
